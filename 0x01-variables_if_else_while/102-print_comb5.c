e47750a984a74df4765603e37fe4e30ca1a9d5d0
Titabel <cdakyen@yahoo.com>
2022-07-01 10:19:12 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,11 +25,9 @@ int main(void)
 																					putchar(32);
 																					putchar((b / 10) + n1);
 																					putchar((b % 10) + n1);
-																					if (a != 98 || b != 99)
-																					{
-																						putchar(com);
-																						putchar(32);
-																					}																	          b += 1;
+																					if (a != 98 || b != 99)																  {
+			      putchar(com);																	putchar(32);
+		      }																	                b += 1;
 		}
 		a += 1;
 	}
