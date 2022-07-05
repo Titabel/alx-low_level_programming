91ce880204f2f25398a59cdec3d76c860e4e24a6
Titabel <cdakyen@yahoo.com>
2022-07-05 13:40:06 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,20 +9,20 @@
  */
 void jack_bauer(void)
 {
-int h;
+        int h;
 
-for (h = 0; h < 24; h++)
-{
-int min;
+        for (h = 0; h < 24; h++)
+        {
+               int min;
 
-for (min = 0; min < 60; min++)
-}
-_putcher('0' + (h / 10));
-_putchar('0' + (h % 10));
-														     _putchar(':');
-														     _putchar('0' + (min / 10));
-														     _putchar('0' + (min % 10));
-														     _putchar('\n');
-}
-}
+               for (min = 0; min < 60; min++)
+               }   
+                       _putcher('0' + (h / 10));
+                       _putchar('0' + (h % 10));
+														                            _putchar(':');
+														                            _putchar('0' + (min / 10));
+														                            _putchar('0' + (min % 10));
+														                            _putchar('\n');
+                }
+       }
 }
