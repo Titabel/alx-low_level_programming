e7033c7d87610703209e4fc78004a53bbed23e8a
Titabel <cdakyen@yahoo.com>
2022-07-05 09:47:05 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,6 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-
+printf("_putchar\n");
 return (0);
 }
