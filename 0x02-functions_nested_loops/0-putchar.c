5ab93cbb7745d507a806f2e49f4bdcc66b15081f
Titabel <cdakyen@yahoo.com>
2022-07-05 07:34:03 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - Prints Holberton
+ * main - Prints Putcher
  *
  * Return: Always 0 (Success)
  */
@@ -14,6 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');								
+        _putchar('\n');
 	 return (0);
-}	
+}
