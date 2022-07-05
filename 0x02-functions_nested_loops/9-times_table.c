0619fda217123df0f2ed102bc70c898acc859bb5
Titabel <cdakyen@yahoo.com>
2022-07-05 14:08:01 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+int ;
+col;
+int product;
+for (r = 0; r <= 9;rint r++)
+{
+for (col = 0; col <= 9; col++)
+{
+product = (r * col);
+if (col == 0)
+{
+_putchar('0' + product);
+}
+else if (product <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + product);
+}
+else if (product > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + (product / 10));
+_putchar('0' + (product % 10));
+}
+}
+_putchar('\n');
+}
+}
