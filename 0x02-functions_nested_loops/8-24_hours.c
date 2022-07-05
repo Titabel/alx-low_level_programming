d0eaeed7bfc534895a7ace8d9eca776aefe9924d
Titabel <cdakyen@yahoo.com>
2022-07-05 13:31:46 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,13 +16,13 @@ for (h = 0; h < 24; h++)
 int min;
 
 for (min = 0; min < 60; min++)
-{														     
-_putchar('0' + (h / 10));															
+}
+_putcher('0' + (h / 10));
 _putchar('0' + (h % 10));
 														     _putchar(':');
 														     _putchar('0' + (min / 10));
 														     _putchar('0' + (min % 10));
 														     _putchar('\n');
-														     }
+}
 }
 }
