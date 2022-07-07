f5751203a7778ddb98564d1d4a77d1a25f403bc4
Titabel <cdakyen@yahoo.com>
2022-07-07 07:40:32 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,12 +11,12 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j =1; j < (size - i); j++)
+		for (j = 1; j < (size - i); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar(35);
-		if (i < (s ize- 1))
-			_putchar('\');
+		if (i < (size - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
