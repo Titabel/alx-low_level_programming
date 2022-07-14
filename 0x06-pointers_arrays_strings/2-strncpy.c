aab7cb7aebf30566b7cc20cf379ea14b29fb497d
Titabel <cdakyen@yahoo.com>
2022-07-14 05:51:59 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
-* _strcmp -> for comparing purpose
-* @s1: param1
-* @s2: param2
-* Return: integer
+* _strncpy -> for copying purpose
+* @dest: param1
+* @src: param2
+* @n: param3
+* Return: string
 */
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int result;
-
-	result = strcmp(s1, s2);
-	return (result);
+	strncpy(dest, src, n);
+	return (dest);
 }
